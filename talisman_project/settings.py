1552ee15e09fb9bf03169630ceaf01ca213ec86b
Edwin <edwinipyana17@gmail.com>
2026-07-02 14:35:58 +0300

    Disable DEBUG and restrict ALLOWED_HOSTS for production security

--- talisman_project/settings.py
+++ talisman_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!lzmlzt9ud-zm4uf8u6)li4j^$kxara%@%=z38r$!g7vttxbqw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['talisman-uwwo.onrender.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
